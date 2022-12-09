4fb418392e08aa84760f1851a2f8bca78fc25a9f
glebite <glebite@gmail.com>
2022-12-08 20:38:29 -0500

    Starting to flesh out the skeleton code.

--- src/skeleton.py
+++ src/skeleton.py
@@ -1,5 +1,25 @@
 """
+skeleton.py - 
 """
+
+
+class Skeleton:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+
+    def add(self):
+        """
+        """
+        pass
+
+    def get(self):
+        """
+        """
+        pass
 
 
 if __name__ == "__main__":
