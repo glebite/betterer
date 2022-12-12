f9ec0b0123537ba8bb4907bd82e3036edf946e09
glebite <glebite@gmail.com>
2022-12-12 18:53:35 -0500

    adding in tests first

---
+++ tests/test_skeleton.py
@@ -0,0 +1,23 @@
+"""
+"""
+import pytest
+
+def test_create_skeleton():
+    pass
+
+
+def test_add_skeleton():
+    pass
+
+
+def test_add_more():
+    pass
+
+
+def test_add_one_more():
+    pass
+
+
+def test_cycle_one_hundred():
+    pass
+
