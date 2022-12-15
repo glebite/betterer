f59b3b800f10e406aece6acdb119e1b1aee4ed5d
glebite <glebite@gmail.com>
2022-12-14 21:03:00 -0500

    Adding more.

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,6 +1,14 @@
-"""
+"""test_skeleton.py
+
+Write tests first, make the code work later.
 """
 import pytest
+
+
+LONG_LIST = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+SHORT_LIST = [1,2,3]
+EMPTY_LIST = []
+
 
 def test_create_skeleton():
     pass
