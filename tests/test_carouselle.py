04c8a1c6cc3519d176c89ceea7c3c0d395c5c67e
glebite <glebite@gmail.com>
2022-12-18 12:49:21 -0500

    Setting things up for initial testing and working with the carouselle (note name change

--- tests/test_carouselle.py
+++ tests/test_carouselle.py
@@ -1,20 +1,20 @@
-"""test_skeleton.py
+"""test_carouselle.py
 
 Write tests first, make the code work later.
 """
 import pytest
 
 
-LONG_LIST = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-SHORT_LIST = [1,2,3]
+LONG_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,  15, 16]
+SHORT_LIST = [1, 2, 3]
 EMPTY_LIST = []
 
 
-def test_create_skeleton():
+def test_create_carouselle():
     pass
 
 
-def test_add_skeleton():
+def test_add_carouselle():
     pass
 
 
